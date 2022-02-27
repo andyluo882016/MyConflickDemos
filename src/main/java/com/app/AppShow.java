a3bc749f8311d323057cf4fb52b7746c858fa700
andyluo882016 <andylu882016@gmail.com>
2022-02-27 13:26:16 -0800

    commit v1 for conflic @ 2/27

--- src/main/java/com/app/AppShow.java
+++ src/main/java/com/app/AppShow.java
@@ -1,8 +1,14 @@
 package com.app;
-
+import com.app.domain.Employee;
 public class AppShow {
 
     public static void main(String[] args){
         System.out.println("Hello My testing confliting");
+        Employee emp=getEmployee("Jeff Smith", 30);
+        System.out.println(emp.toString());
+    }
+
+    public static Employee getEmployee(String name, Integer age){
+        return new Employee(name, age);
     }
 }
