14c0ca05617d586cde84d89e174370b5b9932901
andyluo882016 <andylu882016@gmail.com>
2022-02-27 22:21:38 -0800

    commit v5 for conflic @ 2/27 10:07PM

--- src/main/java/com/app/AppShow.java
+++ src/main/java/com/app/AppShow.java
@@ -1,33 +1,22 @@
 package com.app;
-<<<<<<< HEAD
 
-import com.app.domain.Employee;
-
-public class AppShow {//
-=======
 import com.app.domain.Employee;
 public class AppShow {
->>>>>>> confltest
 
     public static void main(String[] args){//
         System.out.println("Hello My testing confliting");
-<<<<<<< HEAD
+
         System.out.println(show("IU890", "Keven"));
     }
 
     public static Employee show(String id, String name){
         return new Employee(id, name);
-=======
-        Employee emp=getEmployee("Jeff Smith", 30);
-        System.out.println(emp.toString());
     }
 
-    public static Employee getEmployee(String name, Integer age){
-        return new Employee(name, age);
-    }
+
 
     public static String getname(Employee emp){
         return emp.getName();
->>>>>>> confltest
+
     }
 }
--- src/main/java/com/app/domain/Employee.java
+++ src/main/java/com/app/domain/Employee.java
@@ -1,38 +1,20 @@
 package com.app.domain;
 
 public class Employee {//
-<<<<<<< HEAD
+
     private String id;
     private String name;
 
     public String getId() {
         return id;
-=======
-     private String name;
-     private Integer age;
 
-    public Employee(String name, Integer age) {
-        this.name = name;
-        this.age = age;
->>>>>>> confltest
     }
 
     public String getName() {
         return name;
     }
 
-<<<<<<< HEAD
-    public void setId(String id) {
-        this.id = id;
-    }
 
-=======
->>>>>>> confltest
-    public void setName(String name) {
-        this.name = name;
-    }
-
-<<<<<<< HEAD
     public Employee(String id, String name) {
         this.id = id;
         this.name = name;
@@ -42,22 +24,7 @@ public class Employee {//
     public String toString() {
         return "Employee{" +
                 "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-=======
-    public Integer getAge() {
-        return age;
+                ", name='" + name + '\'' ;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-    public Employee(){}
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
->>>>>>> confltest
-                '}';
-    }
 }
