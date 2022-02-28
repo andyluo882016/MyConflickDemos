46dba52eb47a47ad02f67b2d63588420f6bf22c4
andyluo882016 <andylu882016@gmail.com>
2022-02-27 22:06:08 -0800

    commit v5 for conflic @ 2/27 10:07PM

--- src/main/java/com/app/AppShow.java
+++ src/main/java/com/app/AppShow.java
@@ -2,7 +2,7 @@ package com.app;
 
 import com.app.domain.Employee;
 
-public class AppShow {
+public class AppShow {//
 
     public static void main(String[] args){
         System.out.println("Hello My testing confliting");
